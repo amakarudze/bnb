6592758a40906df7a551822abfd53da9df81fcf4
Anna Makarudze <amakarudze@gmail.com>
2024-09-17 21:15:54 +0200

    Add user permissions for staff and guests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,20 @@
 import pytest
 
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
 from django.test import Client
 
 from accounts.models import User
+
+
+def add_permissions(user, group, group_permissions):
+    user.groups.add(group)
+    user.save()
+    user.refresh_from_db()
+    for perm in group_permissions:
+        permission = Permission.objects.get(codename=perm)
+        user.user_permissions.add(permission)
+    user = User.objects.get(id=user.id)
+    return user
 
 
 @pytest.fixture
@@ -30,33 +41,33 @@ def staff_group(db):
 @pytest.fixture
 def guests_group_permissions():
     """Fixture for guest group permissions to test their level of access within the system."""
-    group_permissions = [""]
+    group_permissions = ["add_user", "change_user", "view_user"]
     return group_permissions
 
 
 @pytest.fixture
 def staff_group_permissions():
     """Fixture for front desk staff group to test their level of access within the system."""
-    group_permissions = [""]
+    group_permissions = ["change_user", "view_user"]
     return group_permissions
 
 
 @pytest.fixture
-def guest(db):
+def guest(db, guests_group, guests_group_permissions):
     """Fixture to create a guest user."""
     user = User.objects.create_user(
         email="elsa@test.com", first_name="Elsa", last_name="Doe", password="pass1234"
     )
-    return user
+    return add_permissions(user, guests_group, guests_group_permissions)
 
 
 @pytest.fixture
-def front_desk(db):
+def front_desk(db, staff_group, staff_group_permissions):
     """Fixture to create a front desk staff user."""
     user = User.objects.create_staff(
         email="elias@test.com", first_name="Elias", last_name="Doe", password="pass1234"
     )
-    return user
+    return add_permissions(user, staff_group, staff_group_permissions)
 
 
 @pytest.fixture
