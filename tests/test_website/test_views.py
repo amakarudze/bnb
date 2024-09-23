982e7b9d96e90ec4ff85de4f616a3b1d0bfce60e
Anna Makarudze <amakarudze@gmail.com>
2024-09-23 16:54:49 +0200

    Add tests for authenticated guest

--- tests/test_website/test_views.py
+++ tests/test_website/test_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import reverse
 
 
-def test_home_view_unauthenticated_user(client):
+def test_home_view_unauthenticated_guest(client):
     response = client.get("/")
     assert response.status_code == 200
 
@@ -18,4 +18,9 @@ def test_home_view_manager(manager_client):
 
 def test_home_view_front_desk_staff(front_desk_client):
     response = front_desk_client.get(reverse("website:home"))
+    assert response.status_code == 200
+
+
+def test_home_view_aunthenticated_guest(guest_client):
+    response = guest_client.get(reverse("website:home"))
     assert response.status_code == 200
