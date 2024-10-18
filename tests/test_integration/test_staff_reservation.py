ef3fb955007c5abec01f8d9367c30bd4f1e99120
Anna Makarudze <anna@makarudze.com>
2024-10-18 11:51:49 +0200

    Fix gecko issues

--- tests/test_integration/test_staff_reservation.py
+++ tests/test_integration/test_staff_reservation.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 options = webdriver.FirefoxOptions()
 options.headless = True
